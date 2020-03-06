bb53a57b9a5550691b19964e1b6345d9ead1fb38
Miika Alikirri <miika.alik@gmail.com>
2020-03-06 14:56:30 +0200

    Updated drawer to work with new parser

--- src/drawer.c
+++ src/drawer.c
@@ -2,6 +2,7 @@
 
 #define MAX_WINDOW_WIDTH 80
 #define MAX_MIDDLE_WIDTH (MAX_WINDOW_WIDTH / 2)
+#define MIDDLE_STARTX (MAX_MIDDLE_WIDTH / 2)
 #define centerx(str_len) ((MAX_WINDOW_WIDTH - (str_len)) / 2)
 
 static void draw_to_drawer(drawer* drawer, char* text)
@@ -20,7 +21,7 @@ static void draw_top_navigation(drawer* drawer, html_parser* parser)
 {
     html_link* links = parser->top_navigation;
     // Start length with adding sizes of " | " separators
-    size_t links_len = (TOP_NAVIGATION_SIZE - 1) * 2;
+    size_t links_len = (TOP_NAVIGATION_SIZE - 1) * 3;
     for (size_t i = 0; i < TOP_NAVIGATION_SIZE; i++) {
         links_len += links[i].inner_text.size;
     }
@@ -32,10 +33,12 @@ static void draw_top_navigation(drawer* drawer, html_parser* parser)
         draw_to_drawer(drawer, html_link_text(links[i]));
         drawer->current_x += html_link_text_size(links[i]);
         if (i < TOP_NAVIGATION_SIZE - 1) {
-            draw_to_drawer(drawer, "|");
-            drawer->current_x += 2;
+            draw_to_drawer(drawer, " |");
+            drawer->current_x += 3;
         }
     }
+
+    drawer->current_y += 1;
 }
 
 static void draw_bottom_navigation(drawer* drawer, html_parser* parser)
@@ -43,57 +46,38 @@ static void draw_bottom_navigation(drawer* drawer, html_parser* parser)
     // first row is identical to top navigation
     draw_top_navigation(drawer, parser);
 
-    html_link* links = parser->bottom_navigation;
+    html_row links = parser->bottom_navigation[1];
 
-    int second_row_len = BOTTOM_NAVIGATION_SIZE - TOP_NAVIGATION_SIZE - 3;
-    int second_row_start = TOP_NAVIGATION_SIZE;
+    // int second_row_len = BOTTOM_NAVIGATION_SIZE - TOP_NAVIGATION_SIZE - 3;
+    // int second_row_start = TOP_NAVIGATION_SIZE;
     // Start length with adding sizes of " | " separators
-    int links_len = (second_row_len - 1) * 2;
-    for (int i = second_row_start; i < second_row_start + second_row_len; i++) {
-        links_len += links[i].inner_text.size;
+    size_t links_len = (links.size - 1) * 3;
+    for (size_t i = 0; i < links.size; i++) {
+        links_len += html_link_text_size(html_item_as_link(links.items[i]));
     }
 
     // Draw second navigation row
     drawer->current_y += 1;
     drawer->current_x = (int)centerx(links_len);
-    for (int i = second_row_start; i < second_row_start + second_row_len; i++) {
-        draw_to_drawer(drawer, html_link_text(links[i]));
-        drawer->current_x += html_link_text_size(links[i]);
-        if (i < second_row_start + second_row_len - 1) {
-            draw_to_drawer(drawer, "|");
-            drawer->current_x += 2;
+    for (size_t i = 0; i < links.size; i++) {
+        draw_to_drawer(drawer, html_link_text(html_item_as_link(links.items[i])));
+        drawer->current_x += html_link_text_size(html_item_as_link(links.items[i]));
+        if (i < links.size - 1) {
+            draw_to_drawer(drawer, " |");
+            drawer->current_x += 3;
         }
     }
-}
-
-static size_t middle_row_length(drawer* drawer, html_parser* parser, size_t current, size_t* end)
-{
-    size_t current_row_length = 0;
-    for (; current < parser->middle_size; current++) {
-        size_t item_len = html_item_text_size(parser->middle[current]);
-        current_row_length += item_len;
-        if (item_len + current_row_length >= MAX_MIDDLE_WIDTH)
-            break;
-    }
-
-    *end = current;
-    return current_row_length;
 }
 
 static void draw_middle(drawer* drawer, html_parser* parser)
 {
     html_item_type last_type = HTML_TEXT;
-    size_t end_index = 0;
-    size_t current_index = 0;
-    size_t row_length = 0;
 
-    while (current_index < parser->middle_size) {
-        //printf("While loop\n");
-        row_length = middle_row_length(drawer, parser, current_index, &end_index);
-        drawer->current_x = centerx(row_length);
+    for (size_t i = 0; i < parser->middle_rows; i++) {
+        drawer->current_x = MIDDLE_STARTX;
         drawer->current_y++;
-        for (; current_index <= end_index; current_index++) {
-            html_item item = parser->middle[current_index];
+        for (size_t j = 0; j < parser->middle[i].size; j++) {
+            html_item item = parser->middle[i].items[j];
             if (item.type == HTML_LINK) {
                 if (last_type == HTML_LINK) {
                     drawer->current_x -= 1;
@@ -114,7 +98,6 @@ static void draw_middle(drawer* drawer, html_parser* parser)
 
 void draw_parser(drawer* drawer, html_parser* parser)
 {
-    printw(" middle len: %ld ", parser->middle_size);
     draw_title(drawer, parser);
     draw_top_navigation(drawer, parser);
     draw_middle(drawer, parser);
