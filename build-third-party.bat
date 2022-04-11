947b3cff563a2ac1da992e3d11b640a93adde4f0
Miika Alikirri <miika.alik@gmail.com>
2022-04-11 19:43:44 +0300

    Build using Visual Studio clang tool chain
    This should reduce the chance of getting caught by
    a windows defender false-positive

--- build-third-party.bat
+++ build-third-party.bat
@@ -1,34 +1,41 @@
 @echo off
 
+:: This requires visual studio tools with clang support
+:: Only the x86 Native Tools Command Propmt for VS is supported
+:: See more in https://docs.microsoft.com/en-us/cpp/build/walkthrough-compiling-a-native-cpp-program-on-the-command-line?view=msvc-170
+
 set start_path=%cd%
 
 cd third_party
 
 if not exist SDL (
-    curl -L https://github.com/libsdl-org/SDL/releases/download/release-2.0.20/SDL2-devel-2.0.20-mingw.tar.gz -o SDL.tar.gz
-    tar xf SDL.tar.gz
+    curl -L https://github.com/libsdl-org/SDL/releases/download/release-2.0.20/SDL2-devel-2.0.20-VC.zip -o SDL.zip
+    tar xf SDL.zip
     move SDL2-2.0.20 SDL
-    cd SDL
-    move i686-w64-mingw32\include include
-    cd ..
-    del SDL.tar.gz
+    del SDL.zip
 )
 
 if not exist SDL_ttf (
-    curl -L https://github.com/libsdl-org/SDL_ttf/releases/download/release-2.0.18/SDL2_ttf-devel-2.0.18-mingw.tar.gz -o SDL_ttf.tar.gz
-    tar xf SDL_ttf.tar.gz
+    curl -L https://github.com/libsdl-org/SDL_ttf/releases/download/release-2.0.18/SDL2_ttf-devel-2.0.18-VC.zip -o SDL_ttf.zip
+    tar xf SDL_ttf.zip
     move SDL2_ttf-2.0.18 SDL_ttf
     cd SDL_ttf
-    copy i686-w64-mingw32\include\SDL2\SDL_ttf.h ..\SDL\include\SDL2\SDL_ttf.h
+    copy include\SDL_ttf.h ..\SDL\include\SDL_ttf.h
     cd ..
-    del SDL_ttf.tar.gz
+    del SDL_ttf.zip
 )
 
 if not exist curl (
-    curl -L https://curl.se/windows/dl-7.82.0_4/curl-7.82.0_4-win32-mingw.zip -o curl.zip
-    tar -xf curl.zip
-    move curl-7.82.0-win32-mingw curl
-    del curl.zip
+    curl -L https://github.com/curl/curl/releases/download/curl-7_82_0/curl-7.82.0.tar.gz -o curl.tar.gz
+    tar -xf curl.tar.gz
+    move curl-7.82.0 curl
+    del curl.tar.gz
+    set party_path=%cd%
+    cd curl\winbuild\
+    nmake /f Makefile.vc mode=dll
+    cd ..
+    move builds/libcurl-vc-x86-release-dll-ipv6-sspi-schannel builds/libcurl
+    cd %party_path%
 )
 
 cd %start_path%
--- build-windows.bat
+++ build-windows.bat
@@ -1,24 +1,33 @@
 @echo off
 setlocal enabledelayedexpansion
 
+:: This requires visual studio tools with clang support
+:: Only the x86 Native Tools Command Propmt for VS is supported
+:: See more in https://docs.microsoft.com/en-us/cpp/build/walkthrough-compiling-a-native-cpp-program-on-the-command-line?view=msvc-170
+
 set imgui_files=third_party\imgui\imgui.cpp third_party\imgui\imgui_draw.cpp third_party\imgui\imgui_tables.cpp third_party\imgui\imgui_widgets.cpp third_party\imgui\backends\imgui_impl_sdlrenderer.cpp third_party\imgui\backends\imgui_impl_sdl.cpp
 set imgui_o_files=third_party\imgui\imgui.o third_party\imgui\imgui_draw.o third_party\imgui\imgui_tables.o third_party\imgui\imgui_widgets.o third_party\imgui\backends\imgui_impl_sdlrenderer.o third_party\imgui\backends\imgui_impl_sdl.o
 set C_FILES=src/config.c src/main.c src/printer.c lib/html_parser.c lib/page_loader.c
 set C_O_FILES=src/config.o src/main.o src/printer.o lib/html_parser.o lib/page_loader.o
 set CXX_FILES=src/gui.cpp
-set CFLAGS=-std=c99 -O3 -Wall -Wextra -Werror -Wno-unused-parameter -Wformat-security -Wno-unused-result -pedantic -fPIC -DENABLE_GUI
-set CXXFLAGS=-std=c++11 -O3 -Wall -Wextra -Wno-unused-parameter -Wformat-security -Wno-unused-result -pedantic -fPIC -DENABLE_GUI -mwindows
-set SDL_LINKS=-Lthird_party/SDL/i686-w64-mingw32/lib -lmingw32 -lSDL2main -lSDL2 -Lthird_party/SDL_ttf/i686-w64-mingw32/lib -lSDL2_ttf
-set CURL_LINKS=-Lthird_party/curl/lib -lcurl
-set INCLUDES=-Iinclude -Ithird_party -Ithird_party/imgui -Ithird_party/SDL/include/SDL2 -Ithird_party/curl/include
+:: TODO: add -Werror to CFLAGS and CXX flags
+set CFLAGS=-std=c99 -O3 -Wall -Wextra -Wno-unused-parameter -Wformat-security -Wno-unused-result -pedantic -DENABLE_GUI
+set CXXFLAGS=-std=c++11 -O3 -Wall -Wextra -Wno-unused-parameter -Wformat-security -Wno-unused-result -pedantic -DENABLE_GUI
+set SDL_LINKS=-Lthird_party/SDL/lib/x86 -lSDL2main -lSDL2 -Lthird_party/SDL_ttf/lib/x86 -lSDL2_ttf
+set CURL_LINKS=-Lthird_party/curl/builds/libcurl/lib -llibcurl
+set INCLUDES=-Iinclude -Ithird_party -Ithird_party/imgui -Ithird_party/SDL/include -Ithird_party/curl/builds/libcurl/include
 
 
 (for %%f in (%imgui_files%) do (
     set file=%%f
     set o_file=!file:cpp=o!
+    :: batch files doesn't have the or operator so here we are
     if not exist !o_file! (
         echo Compiling !o_file!
-        g++ %INCLUDES% -c %CXXFLAGS% -o !o_file! !file!
+        clang++ %INCLUDES% -c %CXXFLAGS% -o !o_file! !file!
+    ) else if [%1] == [clean] (
+        echo Compiling !o_file!
+        clang++ %INCLUDES% -c %CXXFLAGS% -o !o_file! !file!
     )
 ))
 
@@ -28,22 +37,21 @@ set INCLUDES=-Iinclude -Ithird_party -Ithird_party/imgui -Ithird_party/SDL/inclu
     :: batch files doesn't have the or operator so here we are
     if not exist !o_file! (
         echo Compiling !o_file!
-        gcc %INCLUDES% -c %CFLAGS% -o !o_file! !file!
+        clang %INCLUDES% -c %CFLAGS% -o !o_file! !file!
     ) else if [%1] == [clean] (
         echo Compiling !o_file!
-        gcc %INCLUDES% -c %CFLAGS% -o !o_file! !file!
+        clang %INCLUDES% -c %CFLAGS% -o !o_file! !file!
     )
 ))
 
 if not exist tekstitv (
     mkdir tekstitv
-    copy third_party\curl\bin\libcurl.dll tekstitv\libcurl.dll
-    copy third_party\SDL\i686-w64-mingw32\bin\SDL2.dll tekstitv\SDL2.dll
-    copy third_party\SDL_ttf\i686-w64-mingw32\bin\SDL2_ttf.dll tekstitv\SDL2_ttf.dll
+    copy third_party\curl\builds\libcurl\bin\libcurl.dll tekstitv\libcurl.dll
+    copy third_party\SDL\lib\x86\SDL2.dll tekstitv\SDL2.dll
+    copy third_party\SDL_ttf\lib\x86\SDL2_ttf.dll tekstitv\SDL2_ttf.dll
     mkdir tekstitv\assets
     xcopy assets tekstitv\assets /E
 )
 
-g++ %INCLUDES% %CXXFLAGS% -mwindows %CXX_FILES% -c -o src/gui.o
-g++ %INCLUDES% %CFLAGS% %imgui_o_files% %C_O_FILES% src/gui.o -static-libgcc -static-libstdc++ %SDL_LINKS% %CURL_LINKS% -o tekstitv/tekstitv.exe
-
+clang++ %INCLUDES% %CXXFLAGS% %CXX_FILES% -c -o src/gui.o
+clang++ %INCLUDES% %CFLAGS% %imgui_o_files% %C_O_FILES% src/gui.o -static %SDL_LINKS% %CURL_LINKS% -o tekstitv/tekstitv.exe
